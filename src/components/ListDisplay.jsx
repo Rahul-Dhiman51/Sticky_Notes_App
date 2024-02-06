1e9eb4723cadd8dda4e46c463fe58de0a5071414
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2024-02-06 22:26:11 +0530

    clickable link

--- src/components/ListDisplay.jsx
+++ src/components/ListDisplay.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Linkify from 'react-linkify';
 
 function ListDisplay({ inputString }) {
   const [isTrue, setIsTrue] = useState(new Array(inputString.split("\n").length).fill(false));
@@ -15,6 +16,12 @@ function ListDisplay({ inputString }) {
   // Split the input string into an array of lines
   const lines = inputString.split("\n").filter((line) => line.trim() !== "");
 
+  const linkDecorator = (href, text, key) => (
+    <a href={href} key={key} target="_blank" rel="noopener noreferrer">
+      {text}
+    </a>
+  );
+
   return (
     <ul>
       {lines.map((line, idx) => (
@@ -22,8 +29,7 @@ function ListDisplay({ inputString }) {
           onClick={()=>HandleOnClick(idx)}
           style={{ textDecoration: isTrue[idx] ? "line-through" : "none", listStyle: "circle"} }
           key={idx}
-        >{line}
-          
+        ><Linkify componentDecorator={linkDecorator}>{line} </Linkify>
         </li>
       ))}
     </ul>
